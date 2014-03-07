5c9d40c0d6156367f63d4b376635c8354cca9f4d
Roger Paffrath <rogerpaffrath@gmail.com>
2014-03-07 11:30:45 -0300

    PLUNGE modifications and teste added

---
+++ teste/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>
+			//O código JavaScript vai aqui
+		</script>
+		<style>
+			/*
+			 * Caso utilize CSS, inclua-o direto no documento
+			 * (não use um arquivo externo)
+			*/
+		</style>
+	</head>
+	<body>
+		<!--
+		
+		O objetivo desse teste é criar um formulário que salve as seguintes
+		informações de um usuário em um banco de dados:
+		
+		- Nome completo
+		- Idade
+		- E-mail
+		
+		Depois de salvar os dados, o formulário deve exibir uma mensagem de sucesso.
+		
+		O salvamento deve ocorrer sem que a página seja recarregada, via Ajax.
+		Já foi incluído o jQuery no cabeçalho, caso queira usar essa biblioteca.
+		
+		Abaixo estão as credenciais de acesso ao banco de dados:
+		
+		host:           testeprogramador.db.6994959.hostedresource.com
+		banco de dados: testeprogramador
+		usuário:        testeprogramador
+		senha:          xp6JbaDhFS!
+		
+		O nome da tabela é "registration" e os campos disponíveis são:
+		
+		id (int)
+		name (varchar)
+		age (int)
+		email (varchar)
+		created (datetime)
+		
+		Boa sorte!
+		
+		-->
+	</body>
+</html>
